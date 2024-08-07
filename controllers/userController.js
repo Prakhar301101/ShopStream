ec10165b338982c24aa186965f4b4a52c247030d
Prakhar Pandey <92363409+Prakhar301101@users.noreply.github.com>
2024-08-07 23:39:02 +0530

    Add DB, Test Auth routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,12 +13,12 @@ module.exports.registerUser = async (req, res) => {
     res.status(400).json({ message: 'Please provide all user details' });
   }
   try {
-    const userExists = await user.find({ username, email });
-    if (userExists) {
+    const userExists = await user.find({ $or: [{ username }, { email }] });
+    if (userExists.length > 0) {
       console.log('try different details');
       return res
         .status(400)
-        .json({ message: 'User already exists, try with different details' });
+        .json({ message: 'User with same username or email exists, try different values' });
     }
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(password, salt);
@@ -38,8 +38,9 @@ module.exports.registerUser = async (req, res) => {
       });
       return res.status(200).json({
         _id: userDoc._id,
-        name: userDoc.name,
+        name: userDoc.username,
         email: userDoc.email,
+        role: userDoc.role,
       });
     } else {
       return res.status(400).json({
@@ -56,91 +57,90 @@ module.exports.registerUser = async (req, res) => {
 // @route   POST /api/user/login
 // @access  Public
 
-module.exports.loginUser=async(req,res)=>{
-    const { username, password } = req.body;
-    if (!username || !password) {
-      console.log('Please provide all user details');
-      res.status(400).json({ message: 'Please provide all user details' });
+module.exports.loginUser = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    console.log('Please provide all user details');
+    res.status(400).json({ message: 'Please provide all user details' });
+  }
+  try {
+    const userDoc = await user.findOne({ username });
+    if (userDoc && (await bcrypt.compare(password,userDoc.password))) {
+      const token = generateToken(userDoc._id, userDoc.name, userDoc.role);
+      res.cookie('jwt', token, {
+        expires: new Date(Date.now() + 25892000000),
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      });
+      return res.status(200).json({
+        _id: userDoc._id,
+        name: userDoc.username,
+      });
+    } else {
+      return res.status(400).json({
+        message: 'Invalid credentials',
+      });
     }
-    try{
-        const userDoc=await user.find({username});
-        if (userDoc && (await bcrypt.compare(password, userDoc.password))) {
-            const token = generateToken(userDoc._id, userDoc.name,userDoc.role);
-            res.cookie('jwt', token, {
-              expires: new Date(Date.now() + 25892000000),
-              httpOnly: true,
-              sameSite: 'none',
-              secure: true,
-            });
-            return res.status(200).json({
-              _id: user._id,
-              name: user.name,
-            });
-          } else {
-            return res.status(400).json({
-              message: 'Invalid credentials',
-            });
-          }
-    }catch(err){
-        console.error(err);
-        return res.status(500).json({ message: 'Server error' });    
-    }
-}
-
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: 'Server error' });
+  }
+};
 
 // @desc    logout user
 // @route   POST /api/user/logout
 // @access  Private
 
-module.exports.logoutUser=async (req,res)=>{
-        res.cookie('jwt', '', {
-          httpOnly: true,
-        })
-        .json({message:'Logout Successful'});
-    }
+module.exports.logoutUser = async (req, res) => {
+  res
+    .cookie('jwt', '', {
+      httpOnly: true,
+    })
+    .json({ message: 'Logout Successful' });
+};
 
 // @desc    Get user details
 // @route   POST /api/user/me
 // @access  Private
 
-module.exports.getDetails=async (req,res)=>{
-    try {
-        const user = await user.findById(req.user.id);
-        if (!user) {
-          return res.status(400).json({ message: 'User not found' });
-        }
-        return res.status(200).json({
-          email: user.email,
-          username: user.name,
-          id: user._id,
-          role:user.role
-        });
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Server error' });
-      }
-}
+module.exports.getDetails = async (req, res) => {
+  try {
+    const userDoc = await user.findById(req.user._id);
+    if (!userDoc) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+    return res.status(200).json({
+      email: userDoc.email,
+      username: userDoc.username,
+      role: userDoc.role,
+      id: userDoc._id
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server error' });
+  }
+};
 
 // @desc    Update user details like (name and email)
-// @route   POST /api/user/update
+// @route   PATCH /api/user/update
 // @access  Private
-module.exports.updateDetails=async (req,res)=>{
-    const data=req.body;
-    try {
-        const userDoc = await user.findById(req.user.id);
-        if (!userDoc) {
-          return res.status(400).json({ message: 'User not found' });
-        }
-        if(data.username) userDoc.username=data.username;
-        if(data.email) userDoc.email=data.email;
-        await userDoc.save();
-        res.status(200).json({message:'Changes saved successfully'});
-      } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Server error' });
-      }
-}
-
+module.exports.updateDetails = async (req, res) => {
+  const data = req.body;
+  try {
+    const userDoc = await user.findById(req.user.id);
+    if (!userDoc) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+    if (data.username) userDoc.username = data.username;
+    if (data.email) userDoc.email = data.email;
+    await userDoc.save();
+    res.status(200).json({ message: 'Changes saved successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Server error' });
+  }
+};
 
 const generateToken = (id, name, role) => {
   return jwt.sign({ id, name, role }, process.env.SECRET, { expiresIn: '30d' });
--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 require('dotenv').config()
 const express=require('express');
 const app=express();
-const PORT=8000;
+const mongoose=require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const PORT=process.env.PORT;
+const userRoutes=require('./routes/userRoutes');
+
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
+mongoose.connect(process.env.MONGO_URI)
+
+app.use(userRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is listening on Port:${PORT}`);
